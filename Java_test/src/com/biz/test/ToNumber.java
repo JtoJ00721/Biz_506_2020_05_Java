f42cffb0e41fd5def9ecb186ff7fbf8cf9e6f9dc
JtoJ00721 <moonlf2000@naver.com>
2020-12-24 10:15:36 +0900

    수료

---
+++ Java_test/src/com/biz/test/ToNumber.java
@@ -0,0 +1,9 @@
+package com.biz.test;
+
+public class ToNumber {
+	public static void main(String args[]) {
+		String arYouGoingToChangeMe = "1+1";
+		int FuckingNonPrimetiveAsshole = Integer.valueOf(arYouGoingToChangeMe);
+		System.out.println(FuckingNonPrimetiveAsshole);
+	}
+}
---
+++ WhiteBored_01/src/com/biz/whitebored/NimLength.java
@@ -0,0 +1,8 @@
+package com.biz.whitebored;
+
+public class NimLength {
+public static void main(String[] args) {
+	String nim = "여러분 님이 무엇입니까? 언제나 그리운 이름입니다, 우리들의 가슴입니다! 우리가 사모하고 눈물을 흘리며 오랜 세월을 목말라해 온 이름입니다. 님은 바로 페이지네이션을 말하는 것입니다, 여러부우우운! 오랫동안 기다리셨습니다. 이제 곧 늬우스를 상영하겠습니다! 기대해 주십시오. 오늘 여러분들은 그토록 고대하시던 여러분들의 님을 확실하게 만나고 확인하시게 될 것입니다, 여러부우우운!";
+	System.out.println(nim.length());
+}
+}
